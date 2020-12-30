4d292d31098bf553972638f5d0120279d7249184
Seschoscho <j.higatzberger@gmail.com>
2020-12-30 21:29:10 +0100

    Soldier

---
+++ Assets/Soldier.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AICreatures
+{
+    public class Soldier : Human
+    {
+        private void Awake()
+        {
+            InitState(new AIStateIdle());
+            InitState(new AIStateWander());
+            InitState(new AIStateFight());
+            InitState(new AIStateChase());
+
+            ChangeState(AIStateIdle.ID);
+        }
+        public override void FinishedState(int state)
+        {
+            if (state == AIStateWander.ID)
+                ChangeState(AIStateIdle.ID);
+
+            if (state == AIStateIdle.ID)
+                ChangeState(AIStateWander.ID);
+
+            if (state == AIStateChase.ID)
+                ChangeState(AIStateFight.ID);
+
+            if (state == AIStateFight.ID)
+            {
+                if(GetTarget() == null)
+                    ChangeState(AIStateIdle.ID);
+                else
+                    ChangeState(AIStateChase.ID);
+            }
+
+        }
+        public override void TargetFound(AICreature target)
+        {
+            base.TargetFound(target);
+            if (currentState.GetID() != AIStateChase.ID)
+                ChangeState(AIStateChase.ID);
+        }
+    }
+}
